906c94635ab78e4032bd8c8bf206bf073dc49320
drryner <drryner@icloud.com>
2020-06-21 23:03:33 +0300

    ADD: basic react-router bindings

---
+++ src/Gimel/Router.js
@@ -0,0 +1,10 @@
+const R = require ("react-router")
+
+exports.classRouter = R.Router
+exports.classBrowserRouter = R.BrowserRouter
+exports.classStaticRouter = R.StaticRouter
+exports.classRoute = R.Route
+exports.classSwitch = R.Switch
+exports.classRedirect = R.Redirect
+exports.classLink = R.Link
+exports.classNavLink = R.NavLink
